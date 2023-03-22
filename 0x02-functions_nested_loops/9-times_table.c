6a2778efefa28627b428f250827c6391a9f6439d
xaranx <arrif.bouchaib03@gmail.com>
2023-03-22 11:08:19 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,13 +22,12 @@ void times_table(void)
 			if (result <= 9)
 			{
 				_putchar(' ');
-				_putchar(result + '0');
 			}
 			else
 			{
 				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
 			}
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
