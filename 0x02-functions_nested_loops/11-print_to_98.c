bece6e9c5618a6079d6dcb5f4aafc0afe56c0ba5
xaranx <arrif.bouchaib03@gmail.com>
2023-03-22 19:39:58 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ *
+ * @n: integer input
+ * Return: n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 0)
+	{		
+		while (n < 98)
+		{
+			if(n < 0)
+			{
+				_putchar('-');
+				_putchar((n * -1) / 10 + '0');
+				_putchar((n * -1) % 10 + '0');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+	else if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n <= 9)
+			{
+				_putchar(n / 10 + '0');			
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n / 100 + '0');
+			_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			n--;
+		}
+		_putchar('\n');
+	}
+}
