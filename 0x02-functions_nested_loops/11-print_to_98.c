efcfef94325c692a01570811dab9bf7565c6587c
xaranx <arrif.bouchaib03@gmail.com>
2023-03-23 04:40:32 -0700

    0. Multiple mains

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -36,8 +36,15 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			if(n <= 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
 		
 			if (n != 98)
 			{
