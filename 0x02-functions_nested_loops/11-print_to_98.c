48f1abb0d6fc4adf148a49559f1af380a105c2cc
xaranx <arrif.bouchaib03@gmail.com>
2023-03-22 19:42:43 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	if (n < 0)
 	{		
-		while (n < 98)
+		while (n <= 98)
 		{
 			if(n < 0)
 			{
@@ -54,7 +54,7 @@ void print_to_98(int n)
 		}
 		_putchar('\n');
 	}
-	if (n >= 98)
+	else if (n >= 98)
 	{
 		while (n >= 98)
 		{
