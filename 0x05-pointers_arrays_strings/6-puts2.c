742bd9d2375bb4def141f376762a06c411a1fc31
xaranx <arrif.bouchaib03@gmail.com>
2023-03-27 17:50:49 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		_putchar(str[i]);
-		i++;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
