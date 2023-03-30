ab39097188a1e374032e2a62be11ee7853f1a0dd
xaranx <arrif.bouchaib03@gmail.com>
2023-03-30 14:54:34 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,60 @@
 #include "main.h"
 
+
+/**
+ * isLower - Detirmine whether the ascii is lowercase
+ *@c : character
+ *Return: 1 if true and 0 if false
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ *isDelimiter - determin whether the ascii is a delimiter
+ *@c : character
+ *Return: 1 if true and 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+
+	char delimiter[] = {"\t\n,.!?\"{}()"};
+
+	for (i = 0; i < 12; i++)
+	{
+		if (c == delimiter[i])
+			return (1);
+	}
+	return (0);
+}
+
 /**
  * cap_string - function that capitalizes all words of a string
  *
- * @str: string toupper
- * Return: char
+ * @s : input string
+ * Return: string with capital words
  */
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimiter = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimiter = 1;
+		else if (isLower(*s) && foundDelimiter)
+		{
+			*s -= 32;
+			foundDelimiter = 0;
+		}
+		else
+			foundDelimiter = 0;
+		s++;
+	}
+	return (ptr);
+}
