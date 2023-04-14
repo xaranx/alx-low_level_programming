194493abaf238581053b6e70b3e954927d6e9a9c
xaranx <arrif.bouchaib03@gmail.com>
2023-04-13 17:13:19 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,36 +5,38 @@
  * string_nconcat -function concatenates two strings
  * @s1: pointer to the first string
  * @s2: pointer to the second string
+ * @n: size of second string
  *  Return: pointer concat 2 string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, s1_len, s2_len;
+	unsigned int i, a, b;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		;
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		;
-	if (!(n >= s2_len))
-		s2_len = n;
-	str = malloc(s1_len + n + 1);
-	if (str == NULL)
-	{
+	a = b = 0;
+	if (s1)
+		for (; s1[a]; a++)
+			;
+	if (s2)
+		for (; s2[b]; b++)
+			;
+	if (!(n >= b))
+		b = n;
+
+	str = malloc(sizeof(char) * (a + b) + 1);
+	if (!str)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-	for (j = 0; j < n; j++)
+
+	for (i = 0; i < (a + b); i++)
 	{
-		str[i] = s2[j];
-		i++;
+		if (i < a)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - a];
 	}
+
 	str[i] = '\0';
+
 	return (str);
 }
