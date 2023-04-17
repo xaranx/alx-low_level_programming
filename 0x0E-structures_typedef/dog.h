5c1d0228f28b0043a0c2c1c2816fc95ef193f56c
xaranx <arrif.bouchaib03@gmail.com>
2023-04-17 16:19:32 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog struct
+ *@name: doge name
+ *@age: Doge age
+ *@owner: Doge owner
+ * Return: owner's name
+ */
+
+
 struct dog
 {
 	 char *name;
