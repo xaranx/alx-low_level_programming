a5309887f74d36fbf53fc6b88f4c159000731905
xaranx <arrif.bouchaib03@gmail.com>
2023-05-02 03:21:34 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - function return the length of the string
- *@s: the string length to check
- * Return: integer of string length
- */
-
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
  * print_list - prints a lists
  *@h: pointero first node
  * Return:size of list
@@ -31,7 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		i++;
 	}
