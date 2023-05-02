235d50bc0247c7fd18bbad6e3fcc41ff199e7dda
xaranx <arrif.bouchaib03@gmail.com>
2023-05-02 14:12:31 -0700

    0. Print list third commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
-
 /**
- * print_listint - print all the element of a list
- *@h: poiter to the head of thelist
- * Return:the number of node in the list
+ * print_listint - prints all the  elements of a listint_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+	return (count);
 }
