51b4b673a4c582c5308bdf6ad31812ad4d41fc65
xaranx <arrif.bouchaib03@gmail.com>
2023-05-02 16:41:32 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: pointer to the pointer of the head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	if (*head != 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
