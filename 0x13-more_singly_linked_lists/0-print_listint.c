b717a4cc9caf60ccf1b67b9c06dafff60691418c
xaranx <arrif.bouchaib03@gmail.com>
2023-05-02 14:18:33 -0700

    0. Print list fourth commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
