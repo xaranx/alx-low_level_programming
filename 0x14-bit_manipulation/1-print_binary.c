e300153f20b68a50af012061128d6097b84490c1
xaranx <arrif.bouchaib03@gmail.com>
2023-05-04 11:29:14 -0700

    0. 0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if
+ * - there is one or more chars in the string b that is not 0 or 1
+ * - b is NULL
+ */
+void print_binary(unsigned long int n)
+{
+
+}
