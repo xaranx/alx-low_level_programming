0026742b79d13b549c644042a0288cc7bc7a23b8
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-07 16:20:29 -0700

    1st task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,18 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: the size, in number of nodes, to make the new hash table.
+ *
+ * Return: a pointer to the newly created hash table.
+ */
+
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
-	unsigned i;
-	
+	unsigned int i;
+
 	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
 	{
